ab4d10fe8c88ed944648cedc41c442b6045e11b7
Ierofantis <evil69669@gmail.com>
2017-01-24 21:26:26 +0200

    minor changes

--- public/game.js
+++ public/game.js
@@ -1,16 +1,23 @@
 
-    var count=5;
+   var count=5;
    var socket = io();
-    socket.on('counts',function(c)
-  { $( "#health" ).html( "Health ==>" + "&nbsp" + c + "" )}  )
+    socket.on('counts',function(c) {
+     $( "#health" ).html( "Health ==>" + "&nbsp" + c + "" 
+      )}  
+     )
 
 $( "#success" ).click(function() { 
-
 socket.emit('counts', count++);
 });
 
 $( "#war" ).click(function() {
- socket.emit('counts', count++);
+ socket.emit('counts', count--);
+ if(count==0){
+  
+  alert("game over")
+  return count=5
+
+ }
 });
 
 jQuery.fn.extend({
